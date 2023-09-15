92e1ef01c270bc62496585782fbb707beaaf6ac4
chenxin <zhr19960307.zhr>
2023-09-15 16:16:05 +0800

    优化边端配置

--- src/main/resources/run-start.bat
+++
@@ -1,3 +0,0 @@
-title zkt-project-control
-chcp 65001
-java -jar -Dfile.encoding=utf-8 -Dspring.profiles.active=Pj1101050001 zkt-project-control.jar
---
+++ src/main/resources/startup.bat
@@ -0,0 +1,3 @@
+title hdwa-project-control
+chcp 65001
+java -jar -Dfile.encoding=utf-8 -Dspring.profiles.active=项目id hdwa-project-control.jar
