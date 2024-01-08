5ba3dfb3f3be37010be618b6b2563d3fe3c4b5b4
何俊宝 <18871082924@163.com>
2024-01-08 14:49:55 +0800

    提交 代码优化

--- src/main/resources/run-start.bat
+++ src/main/resources/run-start.bat
@@ -1,3 +1,3 @@
 title hdwa-project-sdk
 chcp 65001
-java -jar -Xms20g -Xmx20g -Dfile.encoding=utf-8 -Dspring.profiles.active=dev -DprojectId=Pj2102110210 hdwa-project-sdk-1.0.0.jar
+java -jar -Xms12g -Xmx12g -Dfile.encoding=utf-8 -Dspring.profiles.active=dev -DprojectId=Pj2102110210 hdwa-project-sdk-1.0.0.jar
