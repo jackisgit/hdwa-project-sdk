431c5f0ef69b0c4d85ddde6a43bdd79b3f3fcdc2
何俊宝 <18871082924@163.com>
2024-03-14 09:52:39 +0800

    提交 增加计算线程，关闭重算iot

--- src/main/java/com/hdwa/sdk/service/InitialDataService.java
+++ src/main/java/com/hdwa/sdk/service/InitialDataService.java
@@ -335,7 +335,7 @@ public class InitialDataService implements CommandLineRunner {
     /**
      * 刷新数据 重算iot
      */
-    @Scheduled(initialDelay = 1000 * 60 * 5, fixedDelay = 1000 * 60)
+    //@Scheduled(initialDelay = 1000 * 60 * 5, fixedDelay = 1000 * 60)
     public void refreshData() {
         RepositoryImpl repository = DataContainer.projectMap.get(BaseDecConstant.CURRENT_PROJECT_ID);
         if (repository == null) {
